e836db8e097cd98ff2aa95771fff5b23cefaba41
basir5101 <basir.bsmrstu@gmail.com>
2024-02-07 17:33:51 +0600

    done

--- components/Wait.js
+++ components/Wait.js
@@ -36,8 +36,10 @@ export default function Wait() {
                     messages.map((item, index) => (
                         <div key={index} className="chat pb-8 chat-start">
                             <div className="chat-image avatar">
-                                <div className="w-10 rounded-full">
-                                    <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <div className="w-8 text-warning text-xl rounded-full">
+                                    {index === (messages.length - 1) ?
+                                        <div className='bg-error w-8 h-8'></div>
+                                        : <div className='bg-secondary w-8 h-8'></div>}
                                 </div>
                             </div>
                             <div className="chat-bubble">
