c77fb208e2ccb14b33da138673e9796b36a1e52c
basir5101 <basir.bsmrstu@gmail.com>
2024-02-07 14:19:36 +0600

    initial commit

---
+++ components/Facebook.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import Heading from './Heading'
+
+export default function Facebook() {
+    return (
+        <div className='h-screen py-5'>
+            <Heading title='Can I Get Your Facebook Id?' />
+            <ul className="timeline mt-8 timeline-vertical">
+                <li>
+                    <div className="timeline-start  chat chat-end  text-xl">
+                        <div className='chat-bubble chat-bubble-primary '>
+                            ফেসবুক আইডিটা দেয়া যাবে?
+                        </div>
+                    </div>
+
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end chat chat-start font-semibold text-xl">
+                        <div className="chat-bubble chat-bubble-secondary w-96">
+                            Ji
+                        </div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end chat chat-start font-semibold text-xl">
+                        <div className="chat-bubble chat-bubble-secondary w-96">
+                            Wait
+                        </div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end chat chat-start font-semibold text-xl">
+                        <div className="chat-bubble chat-bubble-secondary w-96">
+                            Link dicchi
+                        </div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end chat chat-start font-semibold text-xl">
+                        <div className="chat-bubble chat-bubble-secondary w-96">
+                            www.facebook.com/....
+                        </div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start w-96 chat chat-end  text-xl">
+                        <div className='chat-bubble chat-bubble-primary '>
+                            জি আচ্ছা।
+                        </div>
+                    </div>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <hr />
+                </li>
+            </ul>
+        </div>
+    )
+}
